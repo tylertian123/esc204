b555e107a8061fe99da43f46e08d72c350b33620
Tyler Tian <tylertian123@gmail.com>
2023-03-14 15:07:46 -0400

    Switch to using RPi Pico bare metal SDK

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include <Arduino.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
 
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+int main(void)
+{  
+    stdio_init_all();
+    printf("\n\nHello World\n");
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    while (true)
+    {
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(100);
+    }
 }
