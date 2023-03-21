7c878a04723e6b4940be5ef2b83f81e22ace1eb8
Tyler Tian <tylertian123@gmail.com>
2023-03-21 14:56:07 -0400

    Add LCD display object in UI

--- include/ui.h
+++ include/ui.h
@@ -2,10 +2,12 @@
 
 #include "pico/stdlib.h"
 #include "ui_hw.h"
+#include "pi-pico-LCD/lcd_display.hpp"
 
 namespace ui {
     extern hw::Button b_left, b_right, b_center;
     extern hw::LED led;
+    extern LCDdisplay disp;
 
     void init();
 }
