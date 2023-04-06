476c3ae5ee1dc3eb3efeb6a47be9100bdd9da569
Tyler Tian <tylertian123@gmail.com>
2023-04-06 19:29:59 -0400

    Make some adjustments according to physical prototype
    Change x limit switch to upper limit. Change inverting of steppers.
    Change the servo to use pulse width directly instead of angle
    interpolation.

--- src/subsys.cpp
+++ src/subsys.cpp
@@ -76,7 +76,7 @@ namespace subsys {
     }
 
     XMovement::XMovement() {
-        stepper.add_lower_lim_sw(pinmap::x_lower_lim);
+        stepper.add_upper_lim_sw(pinmap::x_upper_lim);
     }
 
     bool XMovement::busy() const {
@@ -96,8 +96,8 @@ namespace subsys {
     }
 
     void XMovement::calibrate() {
-        stepper.step(INT_MIN);
-        position = 0;
+        stepper.step(INT_MAX);
+        position = hwconf::x_cal_pos;
     }
 
     void XMovement::calibrate_blocking() {
@@ -119,7 +119,7 @@ namespace subsys {
     }
 
     void Gripper::set(Gripper::State state) {
-        servo = state ? hwconf::gripper_open : hwconf::gripper_closed;
+        servo.set_output(state == OPEN ? hwconf::gripper_open : hwconf::gripper_closed);
         busy_until = util::millis() + hwconf::gripper_change_duration;
     }
 
